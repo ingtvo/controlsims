e7c967ad8a0c276268a47a4f6aeacbf234ffbedf
ingtvo <ingtvo@msn.com>
2014-12-03 22:48:51 -0600

    subiendo archivos

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'mysql.hostinger.mx');
+define('DB_USERNAME', 'u877539958_admin');
+define('DB_PASSWORD', 'baker3');
+define('DB_DATABASE', 'u877539958_chips');
+$connection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
